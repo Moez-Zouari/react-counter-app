d39b071d8515d81ec05bbf98704edccb1c6afa6e
Moez <moez_zouari@hotmail.fr>
2021-04-19 15:33:50 +0100

    component - binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,47 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state  = {
-        count : 0,
-         tags: ['tag1', 'tag2', 'tag3']
+        count : 0
      
     };
 
-renderTags(){
-    
-    if (this.state.tags.length === 0) return <p>There are no tags !</p>
+  /*    // Methode 1 , utiliser bind et un constructeur pour  resoudre le probleme de undifiend
+    constructor() {
+      super();
+      this.handleIncrement = this.handleIncrement.bind(this); 
+    } */
 
-    return (
-    <ul> { this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul> );
-}
+    // methode 2 : Utiliser Arrow Function
+    handleIncrement = () => {
+      
+      console.log('Increment Clicked', this);
+    }
 
-  render() {
+    render() {
   
-    return(
-    <div>
-     {this.state.tags.length === 0 && "Please create a new Tag"} 
-     {this.renderTags()}
-    </div>
-    );
+      return(
+        <div> 
+        <span  className={this.getBadgeClasses()}>{this.formatCount()}</span> 
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Incerement</button>
+      </div>
+      );
+    }
+
+    formatCount() {
+      const { count } = this.state;
+    }
+
+    getBadgeClasses() {
+      let classes = "badge m-2 badge-";
+      classes += (this.state.count === 0) ? "warning" : "primary";
+      return classes;
   }
+
+
+  
+
+
+
 
 
 }
