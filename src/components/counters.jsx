c668c785d974cb444bdd741ac2da8cedb449c021
Moez <moez_zouari@hotmail.fr>
2021-04-20 17:07:11 +0100

    cc - single source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,13 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
@@ -20,6 +27,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
