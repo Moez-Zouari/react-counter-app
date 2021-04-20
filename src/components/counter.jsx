2be8177fc54f07c064c955eec51f81e41fdafc3d
Moez <moez_zouari@hotmail.fr>
2021-04-20 14:30:41 +0100

    CC - composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state  = {
-        count : 0
-     
-    };
+  state = {
+    count: 0,
+  };
 
   /*    // Methode 1 , utiliser bind et un constructeur pour  resoudre le probleme de undifiend
     constructor() {
@@ -12,42 +11,37 @@ class Counter extends Component {
       this.handleIncrement = this.handleIncrement.bind(this); 
     } */
 
-    // methode 2 : Utiliser Arrow Function
-    handleIncrement = product => {
-      console.log(product);
-      // Utiliser setState pour afficher le changement de valeur dans UI
-      this.setState({ count: this.state.count + 1})
-    }
+  // methode 2 : Utiliser Arrow Function
+  handleIncrement = (product) => {
+    console.log(product);
+    // Utiliser setState pour afficher le changement de valeur dans UI
+    this.setState({ count: this.state.count + 1 });
+  };
 
-
-    render() {
-  
-      return(
-        <div> 
-        <span  className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-        <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Incerement</button>
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Incerement
+        </button>
       </div>
-      );
-    }
-
-    formatCount() {
-      const { count } = this.state;
-      return    count === 0 ? "Zero" : count;
-    }
-
-    getBadgeClasses() {
-      let classes = "badge m-2 badge-";
-      classes += this.state.count === 0 ? "warning" : "primary";
-      return classes;
+    );
   }
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 
-  
-
-
-
-
-
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 
 export default Counter;
