5422fbf393293c0da6c15eb6af6b30644b822958
Moez <moez_zouari@hotmail.fr>
2021-04-20 16:16:17 +0100

    cc - raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,8 +19,6 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
-
     return (
       <div>
         {this.props.children}
@@ -30,6 +28,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Incerement
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
