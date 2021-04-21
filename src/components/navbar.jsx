35208026000809699bba270218a490532e471531
Moez <moez_zouari@hotmail.fr>
2021-04-21 14:52:21 +0100

    cc - lifting the state up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-        </div>
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
       </nav>
     );
   }
